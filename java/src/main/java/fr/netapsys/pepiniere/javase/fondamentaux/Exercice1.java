6a0dcb71af7a7cb9646a6c404cc4f1418ce092ec
jerdopler <lebaronjerome@free.fr>
2017-11-01 18:00:53 +0100

    Premier envoi sur les fondamentaux

---
+++ java/src/main/java/fr/netapsys/pepiniere/javase/fondamentaux/Exercice1.java
@@ -0,0 +1,30 @@
+package fr.netapsys.pepiniere.javase.fondamentaux;
+
+/**
+ * La classe pour l'exercice 1.
+ * @author Jerome
+ *
+ */
+public class Exercice1 {
+
+	/** Le numéro de l'exercice.*/ 
+	private int numero;
+	
+	/**
+	 * Pour rugir.
+	 * @param roar1 le premier rugissement.
+	 * @param roar2 le second rugissement.
+	 */
+	public void rugissement(final String roar1, final StringBuilder roar2) {
+		// TODO Auto-generated method stub
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+}
---
+++ java/src/test/java/fr/netapsys/pepiniere/javase/fondamentaux/Exercice1Test.java
@@ -0,0 +1,58 @@
+package fr.netapsys.pepiniere.javase.fondamentaux;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Les tests à valider pour l'exercice 1 des fondamentaux JAVA.
+ * 
+ * @author Jerome
+ *
+ */
+public class Exercice1Test {
+
+	/**
+	 * Mettre à jour la méthode roar en ajoutant aux object roar1 et roar2 3 points
+	 * d'exclamations tout en validant les tests.
+	 */
+	@Test
+	public void testRugissement() {
+		// Préparation
+		final String roar1 = "roar";
+		final StringBuilder roar2 = new StringBuilder("roar");
+		new Exercice1().rugissement(roar1, roar2);
+		// Vérifications
+		Assert.assertEquals("Vérification de la valeur de la chaine roar1", "roar", roar1);
+		Assert.assertEquals("Vérification de la valeur de la chaine roar2", "roar!!!", roar2);
+	}
+
+	/**
+	 * Ecrire la méthode deuxRugissements pour valider le test.
+	 */
+	@Test
+	public void testDeuxRugissement() {
+		// Préparation
+		final String roar1 = "roar";
+		final StringBuilder roar2 = new StringBuilder("roar");
+		// TODO méthode à écrire
+		// Vérifications
+		Assert.assertEquals("Vérification de la valeur de la chaine roar1", "roar!!!", roar1);
+		Assert.assertEquals("Vérification de la valeur de la chaine roar2", "roar!!!", roar2);
+	}
+
+	/**
+	 * Ecrire la méthode deuxRugissements pour valider le test.
+	 */
+	@Test
+	public void testReferences() {
+		Exercice1 un = new Exercice1();
+		un.setNumero(1);
+		Exercice1 deux = new Exercice1();
+		Assert.assertTrue("Sans utiliser le setter de deux, vérifier l'égalité", un.getNumero() == deux.getNumero());
+		Assert.assertNull("Vérifier la condition", deux);
+		Exercice1 trois = new Exercice1();
+		Assert.assertNull("Réassigner trois à une référence existante pour vérifier l'égalité", trois);
+		Exercice1 quatre = new Exercice1();
+		Assert.assertTrue("En manipulant la référence quatre, vérifier la condition", un.getNumero() == 4);
+	}
+}
